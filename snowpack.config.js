2691f00d6aec91882b96a5193e75ae03d31a6802
Adam Lovatt <jalovatt@hotmail.com>
2021-05-07 09:14:05 -0600

    fix: updates config for hot reloading

--- snowpack.config.js
+++ snowpack.config.js
@@ -12,12 +12,7 @@ module.exports = {
   env: {
     apikey: '08b1964f2885f6335cd92b41514e33ec',
   },
-  plugins: [
-    '@snowpack/plugin-react-refresh',
-  ],
   devOptions: {
-    // port: 4001,
     open: 'none',
-    hmr: true,
   },
 };
