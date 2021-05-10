548c2ffcd3643d8eadd9602984cd34ae716755b3
Adam Lovatt <jalovatt@hotmail.com>
2021-05-09 20:58:59 -0600

    feat: improves Event formatting

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -3,18 +3,26 @@ import ApiImage from '../ApiImage';
 
 import './styles.css';
 
+const formatEventDate = (date) => new Date(date).toLocaleString('default', { month: 'long', year: 'numeric' });
+
 export default ({ event }) => (
   <div className="EventCard">
     <ApiImage image={event.thumbnail} size="standard_fantastic" alt={event.title} />
     <div className="content">
       <h3>{event.title}</h3>
-      <h4>
-        {event.comics.available}
-        &nbsp;issues, from&nbsp;
-        {event.start?.split(' ')[0] || 'No start date'}
-        &nbsp;to&nbsp;
-        {event.end?.split(' ')[0] || 'No end date'}
-      </h4>
+      <div className="subheading">
+        {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+        <h4>{event.comics.available}&nbsp;issues</h4>
+        {
+          (event.start && event.end)
+            ? (
+              <h4>
+                {`${formatEventDate(event.start)} - ${formatEventDate(event.end)}`}
+              </h4>
+            )
+            : null
+        }
+      </div>
       <p>{event.description}</p>
     </div>
   </div>
